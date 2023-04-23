0df4a39a4906fe44d3b55dcbf220b34a89065f74
FIRST_NAME LAST_NAME <ID@lamduan.mfu.ac.th>
2023-04-23 14:55:33 +0700

    final code Co-authored-by: PawornpratKongdaeng <PawornpratKongdaeng@users.noreply.github.com>

--- src/Reservation.java
+++ src/Reservation.java
@@ -5,19 +5,21 @@ import java.util.Scanner;
 public class Reservation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-        System.out.print(" type some numbers : ");
+        // only staff 
+        System.out.println("Only staff can enter the numbers of tables");
+        System.out.print("Enter number of tables : ");
+        
         int numTables = scanner.nextInt();
         BookingTable bookingTable = new BookingTable(numTables);
-  
+
         while (true) {
             System.out.println("Choose an option:");
             System.out.println("1. Add booking");
             System.out.println("2. Remove booking");
             System.out.println("3. Display bookings");
             System.out.println("4. Exit");
-            
 
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -31,18 +33,24 @@ public class Reservation {
                     System.out.print("Enter name: ");
                     String name = scanner.nextLine();
 
+                    System.out.print("Enter the numbers of people: ");
+                    int peopleNumber = scanner.nextInt();
+                    scanner.nextLine();
+
                     System.out.print("Enter date and time (yyyy-MM-dd HH:mm): ");
                     LocalDateTime dateTime = LocalDateTime.parse(scanner.nextLine(), formatter);
 
-
-                    System.out.println(" Booking details : "  );
-                    System.out.println( "Name: "  + name);
+                    System.out.println("================");
+                    System.out.println("Booking details:");
+                    System.out.println("Name: " + name);
                     System.out.println("Date: " + dateTime);
-
+                    System.out.println("Number of people: " + peopleNumber);
 
                     try {
                         bookingTable.addBooking(tableNumber, name, dateTime);
                         System.out.println("Booking added successfully");
+                        System.out.println("Please come on time.");
+                        System.out.println("================");
                     } catch (IllegalArgumentException e) {
                         System.out.println("Error: " + e.getMessage());
                     }
@@ -66,12 +74,13 @@ public class Reservation {
                     break;
 
                 case 4:
+                    scanner.close();
                     System.exit(0);
-         
+                    break;
+
                 default:
                     System.out.println("Invalid choice");
             }
-            scanner.close();
         }
     }
 }
